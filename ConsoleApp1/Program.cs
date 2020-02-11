92ffbd4482b920c2665bca72cea73338621e79b6
Aline ELLUL <aline.ellul@gmail.com>
2020-02-11 12:48:57 +0100

    test1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("hello du collègue");
             Console.ReadLine();
         }
     }
